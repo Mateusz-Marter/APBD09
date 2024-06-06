d85f410959909e2d435d5f869b3c02f17a83ed7c
Mateusz Marter <mateuszmarter@gmail.com>
2024-06-06 21:49:44 +0200

    first and final

---
+++ APBD09/Context/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.EfConfigurations;
+using WebApplication1.Models;
+
+namespace WebApplication1.Context;
+
+public class AppDbContext : DbContext
+{
+    private IConfiguration _configuration;
+
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public AppDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_configuration["ConnectionStrings:DefaultConnection"]);
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new DoctorConfiguration());
+        modelBuilder.ApplyConfiguration(new MedicamentConfiguration());
+        modelBuilder.ApplyConfiguration(new PatientConfiguration());
+        modelBuilder.ApplyConfiguration(new PrescriptionConfiguration());
+        modelBuilder.ApplyConfiguration(new PrescriptionMedicamentConfiguration());
+    }
+    
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public DbSet<Medicament> Medicaments { get; set; }
+    public DbSet<Prescription_Medicament> PrescriptionMedicaments { get; set; }
+}
---
+++ APBD09/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
+using WebApplication1.Context;
+using WebApplication1.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+builder.Services.AddScoped<AppDbContext, AppDbContext>();
+builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
+
+builder.Services.AddDbContext<AppDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
